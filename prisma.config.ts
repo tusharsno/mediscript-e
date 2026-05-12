f6882761034cb6ce078329e8aaf83eb0210e96f2
Tushar Barua <tusharbarua269@gmail.com>
2026-05-13 02:56:36 +0600

    Fix prisma.config.ts format

--- prisma.config.ts
+++ prisma.config.ts
@@ -3,9 +3,6 @@ import { defineConfig } from "prisma/config";
 
 export default defineConfig({
   datasource: {
-    db: {
-      url: process.env.DATABASE_URL!,
-      directUrl: process.env.DIRECT_URL,
-    },
+    url: process.env.DATABASE_URL!,
   },
 });
